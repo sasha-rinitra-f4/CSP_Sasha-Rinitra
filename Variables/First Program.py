262d4c4688e9c4cbb089ec599eecd8485d4f703a
sasha-rinitra-f4 <sasha.rinitra@ucas-edu.net>
2025-01-31 09:32:53 -0700

    FP Python Practice Assignment

---
+++ Variables/First Program.py
@@ -0,0 +1,6 @@
+# Sasha Rinitra, First Program Python
+
+name = input("Please type in your name: ")
+print(name+ ", Welcome to the Fruit World!")
+fruit = input("What is your fav fruit? ")
+print(fruit+ ", is a great choice!!")
