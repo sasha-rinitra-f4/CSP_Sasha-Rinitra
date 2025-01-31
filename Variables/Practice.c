3c45185ff1fb5cbc353d3dbbbbaed717235a0a34
sasha-rinitra-f4 <sasha.rinitra@ucas-edu.net>
2025-01-31 09:19:28 -0700

    Variables practice in C

---
+++ Variables/Practice.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+char name[] = "Sasha";
+int smallnum = 3;
+int bignumber = 27; 
+char breakfast[] = "Avocado Toast";
+char favcolor[] = "Orange";
+char school[] = "UCAS";
+int year = 2025;
+char eyecolor[] = "Dark Brown";
+int age = 14;
+char favsubject[] = "Math and Biology";
+
+int main(void){
+    printf("%s\n", name);
+    printf("%d\n", smallnum);
+    printf("%d\n", bignumber);
+    printf("%s\n", breakfast);
+    printf("%s\n", favcolor);
+    printf("%s\n", school);
+    printf("%d\n", year);
+    printf("%s\n", eyecolor);
+    printf("%d\n", age);
+    printf("%s\n", favsubject);
+    return 0;
+}
