6ca9b05b1a60cbe81eca21d519f6266cbf33aea2
sasha-rinitra-f4 <sasha.rinitra@ucas-edu.net>
2025-02-04 16:01:01 -0700

    First Program for C Completed

--- Variables/FirstProgram.c
+++ Variables/FirstProgram.c
@@ -5,7 +5,8 @@ char name[12];
 int main(void) {
   printf("Welcome to the Fruit World! Whats your name?");
   scanf("%s\n", name);
-  printf
+  printf("%s\n", name);
+  printf("WELCOME!!!!");
 
   return 0;
 }
