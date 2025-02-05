16fddd3ed5b0f5cd6f8ef497db8958a919fc1b41
sasha-rinitra-f4 <sasha.rinitra@ucas-edu.net>
2025-02-05 09:49:29 -0700

    Finished Expression notes and practices

---
+++ Expressions/Practice.c
@@ -0,0 +1,26 @@
+#include <stdio.h> 
+#include <math.h>
+
+int equation1 = 7-24/8*4+6;
+int equation2 = 18/3-7+2*5;
+int equation3 = 6*4/12+72/8-9;
+int equation4 = 17-6/2+4*3;
+int equation5 = -2*(1*4-2/2)+(6+2-3);
+int equation6 = -1*((3-4*7)/5)-2*24/6;
+int equation7 = (3*pow(5,2)/15)-(5-pow(2,2));
+int equation8 = (pow(1,4)*pow(2,2)+pow(3,3))-pow(2,5)/4;
+int equation9 = pow(22/2-2*5,2)+pow(4-6/6,2);
+
+int main(void){
+    printf("%d\n", equation1);
+    printf("%d\n", equation2);
+    printf("%d\n", equation3);
+    printf("%d\n", equation4);
+    printf("%d\n", equation5);
+    printf("%d\n", equation6);
+    printf("%d\n", equation7);
+    printf("%d\n", equation8);
+    printf("%d\n", equation9);
+
+    return 0;
+}
