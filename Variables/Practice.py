227d13b0eac47386615e4b556638123af5f8f88d
sasha-rinitra-f4 <sasha.rinitra@ucas-edu.net>
2025-02-06 09:23:25 -0700

    Resubmitting Practice Variables for Python

--- Variables/Practice.py
+++ Variables/Practice.py
@@ -1,12 +1,23 @@
 # Sasha Rinitra, Variables Practice Python
 
-name = print("My name is Sasha Rinitra")
-small_number = print("3")
-big_number = print("28")
-breakfast = print("I had Avocado Toast this morning")
-favcolor = print("My fav color is Orange")
-schoolname = print("I go to UCAS")
-year = print("This year is 2025")
-eyecolor = print("My eyecolor is Dark Brown")
-age = print("I am 14 years old")
-fav_subject = print("My fav subjects are Math and Biology")
+name = "My name is Sasha Rinitra"
+small_number = "3"
+big_number = "28"
+breakfast = "I had Avocado Toast this morning"
+favcolor = "My fav color is Orange"
+schoolname = "I go to UCAS"
+year = "This year is 2025"
+eyecolor = "My eyecolor is Dark Brown"
+age = "I am 14 years old"
+fav_subject = "My fav subjects are Math and Biology"
+
+print(name)
+print(small_number)
+print(big_number)
+print(breakfast)
+print(favcolor)
+print(schoolname)
+print(year)
+print(eyecolor)
+print(age)
+print(fav_subject)
