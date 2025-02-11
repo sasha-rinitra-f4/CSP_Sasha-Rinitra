a61c27754c400fa80c192c10edc043c3f1264bd3
sasha-rinitra-f4 <sasha.rinitra@ucas-edu.net>
2025-02-11 16:29:35 -0700

    Finsihed working on python name decorator, started on C

---
+++ Strings/Name Decorator.py
@@ -0,0 +1,7 @@
+# Sasha Rinitra, Name Decorator Python
+
+name = input("What is your name?")
+dec1 = "~~~"
+dec2 = "~~~"
+
+print(dec1+name+dec2)
---
+++ Strings/Name_Decorator.c
@@ -0,0 +1,12 @@
+// Sasha Rinitra, Name Decorator C
+#include <stdio.h>
+#include <string.h>
+
+float name;
+
+int main(void) {
+    printf("What is your name?");
+    scanf("");
+
+  return 0;
+}
