2e330d61b44213b47c070599b2f050e3b436c66c
sasha-rinitra-f4 <sasha.rinitra@ucas-edu.net>
2025-02-11 16:12:09 -0700

    Made some corrections to financial calc C

--- Expressions/Financial_Calculator.c
+++ Expressions/Financial_Calculator.c
@@ -21,19 +21,19 @@ int main(void){
     printf("%s\n", welcome);
 
     printf("What is your monthly income?\n");
-    scanf("%d", &income_amount);
+    scanf("%f", &income_amount);
 
     printf("How much do you pay for rent every month?\n");
-    scanf("%d", &rent_amount);
+    scanf("%f", &rent_amount);
 
     printf("How much do you spend for utilities every month?\n");
-    scanf("%d", &utilities_amount);
+    scanf("%f", &utilities_amount);
 
     printf("How much do you spend on groceries every month?\n");
-    scanf("%d", &groceries_amount);
+    scanf("%f", &groceries_amount);
 
     printf("How much do you spend on transportation every month?\n");
-    scanf("%d", &transportation_amount);
+    scanf("%f", &transportation_amount);
 
     float savings = income_amount*0.1;
     float income_spending = rent_amount+utilities_amount+groceries_amount+transportation_amount+savings;
@@ -45,12 +45,12 @@ int main(void){
     float percent_spending_income = (income_spending/income_amount)*100;
     float percent_savings = (savings/income_amount)*100;
 
-    printf("The amount of money you spend on rent is %.2f%% of your income\n", percent_rent);
-    printf("The amount of money you spend on utilities is %.2f%% of your income\n", percent_utilities);
-    printf("The amount of money you spend on groceries is %.2f%% of your income\n", percent_groceries);
-    printf("The amount of money you spend on transportation is %.2f%% of your income\n", percent_transportation);
-    printf("Your total amount of money spent is %.2f%% of your income\n", percent_spending_income);
-    printf("You save %.1f%% of your income\n", percent_savings);
+    printf("The amount of money you spend on rent is $%.2f and that is %.2f%% of your income\n", rent_amount, percent_rent);
+    printf("The amount of money you spend on utilities is $%.2f and that is %.2f%% of your income\n", utilities_amount, percent_utilities);
+    printf("The amount of money you spend on groceries is $%.2f and that is %.2f%% of your income\n", groceries_amount, percent_groceries);
+    printf("The amount of money you spend on transportation is $%.2f and that is %.2f%% of your income\n", transportation_amount, percent_transportation);
+    printf("The amount of money you spend on a total of $%.2f and that is %.2f%% of your income\n", income_spending, percent_spending_income);
+    printf("You save $%.2f which is %.1f%% of your income\n", savings, percent_savings);
     
     return 0;
 }
