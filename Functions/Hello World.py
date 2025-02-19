1ff4a4ad695bf88e46d46327bc8b8ee32c9fa4ab
sasha-rinitra-f4 <sasha.rinitra@ucas-edu.net>
2025-02-19 09:49:27 -0700

    Finished Updatind Hello Wrld Py, working on C

---
+++ Functions/Hello World.py
@@ -0,0 +1,16 @@
+# Sasha Rinitra, Update Hello World Python
+
+def user(word):
+    return input(f"Tell me a {word}:\n")
+     
+
+name1 = user("name")
+print(f"Welcome to Hello World {name1}\n")
+name2 = user("name")
+print(f"Welcome to Hello World {name2}\n")
+name3 = user("name")
+print(f"Welcome to Hello World {name3}\n")
+name4 = user("name")
+print(f"Welcome to Hello World {name4}\n")
+name5 = user("name")
+print(f"Welcome to Hello World {name5}\n")
---
+++ Functions/Hello_World.c
@@ -0,0 +1,15 @@
+// Sasha Rinitra, Update Hello World C
+#include <stdio.h>
+char add(char type[20]){
+    printf("%s\n", name);
+    
+}
+
+char name[20];
+int main(void) {
+    printf("Hello user, Please tell me your name:\n"); 
+    scanf("%s", name);
+
+        
+  return 0;
+}
