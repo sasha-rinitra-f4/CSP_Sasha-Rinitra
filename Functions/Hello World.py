cb0509b3ff7f18b1f94e3bee5761c1dd9384575d
sasha-rinitra-f4 <sasha.rinitra@ucas-edu.net>
2025-02-21 09:15:03 -0700

    Resubmit UHW Python... again

--- Functions/Hello World.py
+++ Functions/Hello World.py
@@ -1,9 +1,9 @@
 # Sasha Rinitra, Update Hello World Python
 
 def user(word):
-    return print(f"Hello {word}\n")
-name1 = user("Berin")
-name2 = user("Josie")
-name3 = user("William")
-name4 = user("Maren")
-name5 = user("Aven")
+    return "Hello " + word
+print(user("Berin"))
+print(user("Josie"))
+print(user("William"))
+print(user("Maren"))
+print(user("Aven"))
