33474257eeab5b9b57b13744cab6bfcf87d92edc
sasha-rinitra-f4 <sasha.rinitra@ucas-edu.net>
2025-02-24 09:53:37 -0700

    Worked on Conditonal Notes pY

---
+++ Conditionals/Notes.py
@@ -0,0 +1,38 @@
+# Sasha Rinitra, Conditional Notes Python
+
+# print("Hello, welcome to my program that will sort you into a category.")
+
+# name = input("What is your name?\n")
+
+# if name == "Vienna":
+    #print("Oh you are the teacher.... never mind")
+#else:
+    #print("You are a student. Welcome to class.")
+# 2 equal signs (==) will check to see if the content and the dtat type is the same
+
+
+
+#num = int(input("How many would you like: (give me a number above 0)\n")) # change the number here when running code
+
+#if num == 1: #the first condition has to be the least likely
+    #print("You have asked for a item")
+#elif num <= 3:
+    #print("You have asked for a couple of the items.")
+#elif num <= 5:
+    #print("You have asked for a few of the items...or you are in the south and you asked for a few.")
+#else:
+    #print("You have asked for couple of the few items")
+
+
+
+name = "Katie"
+
+if "a" in name:
+    print("Your name has the letter A!")
+else:
+    print("Your name doesn't have the letter A.")
+
+if "a" in name or "e" in name or "i" in name or "o" in name or "u" in name:
+    print("Your name has a vowel!")
+else:
+    print("Your name doesn't have a vowel.")
