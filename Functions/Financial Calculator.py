02193a4ea39dbc68b6e52dff54ce62d9577b7874
sasha-rinitra-f4 <sasha.rinitra@ucas-edu.net>
2025-02-24 16:05:09 -0700

    Finished updating Financial Calc Python

--- Functions/Financial Calculator.py
+++ Functions/Financial Calculator.py
@@ -4,26 +4,25 @@
 print("Welcome to Financial Calculator!")
 
 def user(type):
-    print(input(f"How much is your monthly {type}?"))
+    return float(input(f"How much is your monthly {type}? \n"))
     
-print(user("income"))
-print(user("rent"))
-print(user("utilities"))
-print(user("groceries"))
-print(user("transporation"))
+income = user("income")
+rent = user("rent")
+utilities = user("utilities")
+groceries = user("groceries")
+transportation = user("transporation")
 
 savings = income*0.1
-# Calculate spending money, income - (rent+utilities+groceries+transportations+savings) (variable)
 expenses = rent+utilities+groceries+transportation+savings
 
 def info(income, amount, type):
-    percent_type = amount/income*100
-    print(f"You spend ${amount:.2f} on {type} and that is {percent_type:.1f} of your income")
+    percent_type = float(amount/income*100)
+    print(f"You spend ${amount:.2f} on {type} and that is {percent_type:.1f}% of your income")
 
-income, rent, utilities, groceries, transportation = float(input(f"What is your monthly {type}"))
 info(income, rent, "rent")
 info(income, utilities, "utilities")
 info(income, groceries, "groceries")
 info(income, transportation, "transportation")
 info(income, savings, "savings")
 
+print(f"You saved ${savings:.2f} and that is 10% of your income")
