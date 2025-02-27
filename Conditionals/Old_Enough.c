91592be2564c6ffd2a54ce56d9b6c25596123024
sasha-rinitra-f4 <sasha.rinitra@ucas-edu.net>
2025-02-27 09:49:32 -0700

    Updated Old Enough C

--- Conditionals/Old_Enough.c
+++ Conditionals/Old_Enough.c
@@ -14,6 +14,8 @@ int main(void){
     printf("You are eligible to go to school! But you can't get your permit, drive or vote:(");
   }else if (age == 17){
     printf("You are eligible to get your learners permit, go to school and drive but you can't vote yet:(");
+  }else if (age <= 4){
+    printf("You are not eligible to get your learners permit, go to school, drive or vote yet:(");
   }else{
     printf("You are eligible to get your permit, drive, go to school and vote!");
     }
