8bb00e92b58968744e6538e94b0e48efa5e3bcf0
sasha-rinitra-f4 <sasha.rinitra@ucas-edu.net>
2025-02-28 09:17:45 -0700

    Finished old enough for C

--- Conditionals/Old_Enough.c
+++ Conditionals/Old_Enough.c
@@ -10,14 +10,20 @@ int main(void){
   scanf("%d", &age);
 
 
-  if(age >= 15){
-    printf("You are eligible to go to school, get your permit, drive or vote:(");
-  }else if (age >= 17){
-    printf("You are eligible to get your learners permit, go to school and drive but you can't vote yet:(");
-  }else if (age >= 18){
-    printf("You are eligible to get your learners permit, go to school, drive and vote!");
+  if(age < 5){
+    printf("You are not eligible to go to school, get your permit, drive or vote yet:(");
+  }else if (age == 5){
+    printf("You aren't eligible to get your learners permit, vote and drive but you can go to school!");
+  }else if (age <= 14){
+    printf("You aren't eligible to get your learners permit, vote and drive but you can go to school!");
+    }else if (age == 15){
+    printf("You are  eligible to get your permit, drive, go to school but can't vote yet:(");
+    }else if (age == 16){
+    printf("You are  eligible to get your permit, drive, go to school but can't vote yet:(");
+    }else if (age == 17){
+    printf("You are  eligible to get your permit, drive, go to school but can't vote yet:(");
     }else{
-    printf("You are not eligible to get your permit, drive, go to school and vote :(");
+      printf("You are eligible to go to school, get your permit, drive and vote!");
     }
 
   return 0;
