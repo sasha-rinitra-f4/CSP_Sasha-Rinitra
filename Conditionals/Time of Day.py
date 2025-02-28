7c179bbda6cff3c7506e3095f5ab8335c23c6e3a
sasha-rinitra-f4 <sasha.rinitra@ucas-edu.net>
2025-02-28 09:28:05 -0700

    Finished Time of day in python

--- Conditionals/Time of Day.py
+++ Conditionals/Time of Day.py
@@ -1,2 +1,17 @@
 #Sasha Rinitra, Time of day Python
 
+import time
+
+current = time.time()
+now = time.ctime(current)
+local_time = time.localtime(current)
+hour = local_time.tm_hour
+
+if hour >= 12 and hour <= 16:
+    print("Good afternoon!!")
+elif hour > 16 and hour <= 19:
+    print("Good Evening!!")
+elif hour > 19:
+    print("Good night!!")
+else:
+    print("Good morning!!")
