bbf16c745121d212e371df934c65b775430df1e0
sasha-rinitra-f4 <sasha.rinitra@ucas-edu.net>
2025-03-06 15:55:06 -0700

    Finished FizzBuzz C

--- Loops/Fizzbuzz.c
+++ Loops/Fizzbuzz.c
@@ -3,12 +3,12 @@
 int num;
 int main(void) {
   for(num=0; num<51; num++){
-    if(num % 3){
-        printf("Fizz\n");
-    }else if(num % 5){
-        printf("Buzz\n");
-    }else if(num % 3 && num % 5){
+    if(num % 3 == 0 && num % 5 == 0){
         printf("FizzBuzz\n");
+    }else if(num % 5 == 0){
+        printf("Buzz\n");
+    }else if (num % 3 == 0){
+        printf("Fizz\n");
     }else{
         printf("%d\n", num);
     }
