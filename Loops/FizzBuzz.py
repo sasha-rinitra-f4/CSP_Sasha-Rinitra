1bb876f93e389dea58afd39e0b5ff0a382600b2d
sasha-rinitra-f4 <sasha.rinitra@ucas-edu.net>
2025-03-06 15:49:07 -0700

    Finished FizzBuzz for python

--- Loops/FizzBuzz.py
+++ Loops/FizzBuzz.py
@@ -1,11 +1,11 @@
 #Sasha Rinitra, FizzBuzz Python
-num = 0
+
 for num in range(1,51):
-    if num % 3 and num % 5:
+    if num % 3 == 0 and num % 5 == 0:
         print("FizzBuzz")
-    elif num % 3:
-        print("Fizz")
-    elif num % 5:
+    elif num % 5 == 0:
         print("Buzz")
+    elif num % 3 == 0:
+        print("Fizz")
     else:
         print(num)
