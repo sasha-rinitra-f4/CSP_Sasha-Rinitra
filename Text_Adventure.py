3470f561d2a5ced1cf5fce5d4e79b64e18bad680
sasha-rinitra-f4 <sasha.rinitra@ucas-edu.net>
2025-03-20 15:53:03 +0000

    Started coding
    Co-authored-by: josephine-chiu <josephine-chiu@users.noreply.github.com>
    Co-authored-by: malu-estevam <malu-estevam@users.noreply.github.com>
    Co-authored-by: adelleponczoch <adelleponczoch@users.noreply.github.com>

--- Text_Adventure.py
+++ Text_Adventure.py
@@ -1,2 +1,7 @@
-#Sasha Rinitra, Josephine Chiu, Adelle Ponczoch, Malu Estevam: Text Based Adventure in Py hon
+#Sasha Rinitra, Josephine Chiu, Adelle Ponczoch, Malu Estevam: Text Based Adventure in Python
 
+# Requirements:
+    # A variable
+    # 2 Functions → 10 pts, Program has 2 connected functions
+    # A loop
+    # A conditional → 10 pts, Conditional has at least 3 different outputs
