2b950ed307572148c7eb4c274e6f73a8e50371c1
sasha-rinitra-f4 <sasha.rinitra@ucas-edu.net>
2025-03-25 22:42:53 +0000

    Almost done with the code just little fixes Co-authored-by: josephine-chiu <josephine-chiu@users.noreply.github.com> Co-authored-by: malu-estevam <malu-estevam@users.noreply.github.com> Co-authored-by: adelleponczoch <adelleponczoch@users.noreply.github.com>

--- Text_Adventure.py
+++ Text_Adventure.py
@@ -1,101 +1,131 @@
 #Sasha Rinitra, Josephine Chiu, Adelle Ponczoch, Malu Estevam: Text Based Adventure in Python- Stranded on
 
-
-# Requirements:
-    # A variable
-    # 2 Functions → 10 pts, Program has 2 connected functions
-    # A loop
-    # A conditional → 10 pts, Conditional has at least 3 different outputs
-
-# Malu and Sasha worked on this while loop with conditionals
-while True:  
-    print("Oh no! You are the sole survivor of a terrible shipwreck and you are stranded on an island. You must make tough choices to go home. You will be given 3 choices for every situation and only 2 of them will be right, if you die, you have to restart. Good luck! Hope you survive and get home!")  
-      
-    # Get the first choice  
-    day_one = input("Are you hungry, thirsty, or do you want to find shelter?\n").lower()  
-      
-    # DAY ONE  
-    if day_one == "shelter":  
-        shelter_choice = input("Do you choose leaves, bark, or a cave?\n").lower()  
-        if shelter_choice == "leaves" or shelter_choice == "bark":  
-            print("You survived, keep going.\n")  
-        elif shelter_choice == "cave":  
-            print("You got eaten by a bear. Restart.\n")  
-            break  # Ends the loop, player has to restart  
+# DAY ONE- Sasha worked on the while loop
+# Adelle did the function
+# Malu did the conditional
+def day1():
+    while True:  
+        # print statement introduces our program.t
+        print("Oh no! You are the sole survivor of a terrible shipwreck and you are stranded on an island. You must make tough choices to go home. You will be given 3 choices for every situation and only 2 of them will be right, if you die, you have to restart that day. Good luck! Hope you survive and get home!")  
+        #This creates a variab
+        day_one = input("Are you hungry, thirsty, or do you want to find shelter?\n").lower()  
+        if day_one == "shelter" or day_one == "find shelter":  
+            shelter_choice = input("Do you choose leaves, bark, or a cave?\n").lower()  
+            if shelter_choice == "leaves" or shelter_choice == "bark":  
+                print("You survived, keep going.\n")
+                break  # Ends the loop, player has to restart   
+            elif shelter_choice == "cave":  
+                print("You got eaten by a bear. Restart.\n")  
+            else:  
+                print("That is not an option. Choose again. \n")  
+        # This is the elif of the c
+        elif day_one == "hungry":  
+            hungry_choice = input("Do you want berries, nuts, or insects?\n").lower()  
+            if hungry_choice == "nuts" or hungry_choice == "insects":  
+                print("You survived, keep going.\n")
+                break  # Ends the loop, player has to restart   
+            elif hungry_choice == "berries":  
+                print("The berries were poisonous. Choose again, you died.\n")  
+            else:  
+                print("That is not an option. Choose again.\n")  
+                
+        elif day_one == "thirsty":  
+            thirsty_choice = input("Do you want to go to a river, lake, or pond?\n").lower()  
+            if thirsty_choice == "river" or thirsty_choice == "lake":  
+                print("You survived, keep going.\n")
+                break  # Ends the loop, player has to restart  
+            elif thirsty_choice == "pond":  
+                print("That water was nasty. Choose again.\n")  
+            else:  
+                print("Not an option. Choose again.\n")  
         else:  
-            print("That is not an option.\n")  
-          
-    elif day_one == "hungry":  
-        hungry_choice = input("Do you want berries, nuts, or insects?\n").lower()  
-        if hungry_choice == "nuts" or hungry_choice == "insects":  
-            print("You survived, keep going.\n")  
-        elif hungry_choice == "berries":  
-            print("The berries were poisonous. Restart, you died.\n")  
-            break  # Ends the loop, player has to restart  
+            print("That is not an option. Choose again.\n")  
+            break  # Ends the loop   
+    
+
+# DAY TWO- Sasha worked on the loop
+# Josephine did the function
+# Malu did the conditional
+
+
+def day2(): #the function
+    while True: #the loop is  for when the user chooses a wrong choice and has to restart   
+        day_two = input("It's the next day and you survived the first night. Keep going. Now, do you want to find civilization, gather weapons, or signal for help?\n")
+        if day_two == "find civilization":  
+            civ_choice = input("Do you choose  hill trail, river trail, or forest trail?\n").lower()  
+            if civ_choice == "forest trail" or civ_choice == "hill trail":  
+                print("You survived, keep going.\n")
+                break  # Ends the loop, player has to restart   
+            elif civ_choice == "river trail":  
+                print("The water was too deep for you to swim in, the currents killed you. Choose again!\n")  
+            else:  
+                print("That is not an option. Choose again.\n") 
+                 
+        elif day_two == "gather weapons":  
+            weapon_choice = input("Do you want to collect an axe, net, or spear?\n").lower()  
+            if weapon_choice == "axe" or weapon_choice == "net":  
+                print("You survived, keep going.\n")
+                break  # Ends the loop, player has to restart   
+            elif weapon_choice == "spear":  
+                print(" You accidentally stabbed yourself, that’s embarrassing. Choose again.\n")  
+            else:  
+                print("That is not an option.\n") 
+                 
+        elif day_two == "signal for help":  
+            help_choice = input("If you want to signal for help, do you want to, gather leaves to create smoke, run to the beach and scream for help, or build a giant SOS sign with rocks?\n").lower()  
+            if help_choice == "gather leaves to create smoke" or help_choice == "build a giant SOS sign with rocks":  
+                print("You survived, keep going.\n")
+                break  # Ends the loop, player has to restart  
+            elif help_choice == "run to the beach and scream for help":  
+                print("A unicorn heard you and stabbed you with its horn. Your scream was so ugly. Choose again!\n")  
+            else:  
+                print("Not an option.\n")  
+                
         else:  
-            print("That is not an option.\n")  
-  
-    elif day_one == "thirsty":  
-        thirsty_choice = input("Do you want to go to a river, lake, or pond?\n").lower()  
-        if thirsty_choice == "river" or thirsty_choice == "lake":  
-            print("You survived, keep going.\n")  
-        elif thirsty_choice == "pond":  
-            print("That water was nasty. You died, restart.\n")  
-            break  # Ends the loop, player has to restart  
+            print("That is not an option\n")  
+            break  # Ends the loop  
+
+#DAY THREE- Malu wrote the conditional part, Ad and Josephine did the function, Sasha wrote the while loop
+
+def day3():    
+    while True:
+        day_three = input("There is a tsunami, do you want to climb a tree, go to shelter, or run for your life\n").lower() 
+        if day_three == "climb a tree":  
+            tree_choice = input("Do you climb an oak, bamboo, or palm tree\n").lower()  
+            if tree_choice == "oak" or tree_choice == "palm":  
+                print("You survived, on every single level. Help will now come! You win\n")
+                break  # Ends the loop, player has to restart   
+            elif tree_choice == "bamboo":  
+                print("You died. That tree was weaker than you. Choose again, bud.\n")  
+            else:  
+                print("That is not an option. Choose again.\n")  
+                
+        elif day_three == "go to shelter":  
+            shelters_choice = input("Do you want to go to a cliff over hang, tunnel, or climb a mountain? \n").lower()  
+            if shelters_choice == "cliff over hang" or shelters_choice == "mountain":  
+                print("You survived, on every single level. Help will now come! Winner winner, chicken dinner.\n")
+                break  # Ends the loop, player has to restart   
+            elif shelters_choice == "tunnel":  
+                print("The berries were poisonous. Choose again, you died.\n")  
+            else:  
+                print("That is not an option. Choose again.\n")  
+                
+        elif day_three == "run for your life" or day_three == "run":  
+            life_choice = input("Do you run in the opposite direction of the tsunami, into a dense forest, or into the tsunami\n").lower()  
+            if life_choice == "opposite direction" or life_choice == "dense forest":  
+                print("You survived, on every single level. Help will now come! You win\n")
+                break  # Ends the loop, player has to restart  
+            elif life_choice == "into the tsunami":  
+                print("I don’t know if you have a death wish or are just dumb, but you’re dead. Restrart.\n")  
+            else:  
+                print("Not an option. Choose again.\n") 
+                break 
+                
         else:  
-            print("Not an option.\n")  
-    else:  
-        print("That is not an option\n")  
-        break  # Ends the loop     
-
-# DAY TWO
-print("It's the next day and you survived the first night. Keep going. Now, do you want to find civilization, gather weapons, or signal for help?")
-day_two = ["find civilization", "gather wapons", "signal for help"]
-
-
-
-if day_two == "civilization":
-    input("Do you want to take a hill trail, river trail, or forest trail?\n")
-elif day_two == "gather weapons":
-    input("Do you want an axe, a spear, or a net?\n")
-elif day_two == "signal for help":
-    input("Do you want to gather leaves to create smoke, run to the beach and scream for help, or build a giant SOS sign with rocks?\n")
-
-
-# DAY THREE
-
-print("It’s the third day, if you survive today, help will come. A tsunami is coming!")
-day_three = ["climb a tree", "go to shelter", "run for your life"]
-
-# Adelle did the following input + parameter function
-#def user(day3):
-    #return (input(f"Do you want {day3}"))"Do you want "
-#print(input("to climb a palm tree, an oak tree, or a bamboo tree?"))
-
-#def user(word):
-#    return input(f"Do you want {word}:\n")
-#food = user("food")
-#water = user("water")
-#shelter = user("shelter")
-
-
-# Josephine did the following function:
-#def result(choices):
-#    return "You survived! " + choices
-#print(result())
-#print(result("You chose leaves."))
-#print(result(choices))
-
-# Josephine did the following function:
-#def input(choices):
-#    return "Do you want " + choices + "?"
-#print(input("food, water, or shelter"))
-#print(input("to find civilization, gather weapons, or signal for help"))
-#print(input("to find leaves, bark, or a cave"))
-#print(input("to find berries, nuts, or insects"))
-#print(input("to go to a river, lake, or pond"))
-
-
-
-   
-  
+            print("That is not an option. Choose again.\n")  
+            break  # Ends the loop
+            
+# calling the functions so that each day can be called in the correct order- called by adelle and josephine
+day1()
+day2()
+day3()
